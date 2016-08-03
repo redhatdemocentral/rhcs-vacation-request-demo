b614895fa00c7edb1a41df6ec89622794a2675f4
Eric D. Schabell <eric@schabell.org>
2016-08-03 10:38:28 -0400

    Upated to bpmsuite version 6.3.

--- init.bat
+++ init.bat
@@ -9,9 +9,9 @@ set SRC_DIR=%PROJECT_HOME%installs
 set OPENSHIFT_USER=openshift-dev
 set OPENSHIFT_PWD=devel
 set WEBSERVICE=vacation.war
-set BPMS=jboss-bpmsuite-installer-6.2.0.BZ-1299002.jar
+set BPMS=jboss-bpmsuite-6.3.0.GA-installer.jar
 set EAP=jboss-eap-6.4.0-installer.jar
-set EAP_PATCH=jboss-eap-6.4.4-patch.zip
+set EAP_PATCH=jboss-eap-6.4.7-patch.zip
 
 REM wipe screen.
 cls
@@ -110,6 +110,9 @@ if not "%ERRORLEVEL%" == "0" (
 	GOTO :EOF
 )
 
+REM need to wait a bit for new build to finish with developer image.
+timeout 3 /nobreak
+
 echo.
 echo Importing developer image...
 echo.
@@ -125,7 +128,7 @@ if not "%ERRORLEVEL%" == "0" (
 echo.
 echo Starting a build, this takes some time to upload all of the product sources for build...
 echo.
-call oc start-build rhcs-vacation-demo --from-dir=. --follow=true
+call oc start-build rhcs-vacation-demo --from-dir=. --follow=true --wait=true
 
 if not "%ERRORLEVEL%" == "0" (
   echo.
